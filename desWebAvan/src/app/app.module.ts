e4be6b1a6e2470ecaa245b51b6a160f9eda54ec3
Anthony Nova <anthony.nova@tecsup.edu.pe>
2022-10-24 09:32:17 -0500

    primera funcionalidad

---
+++ desWebAvan/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TrabajadorComponent } from './trabajador/trabajador.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TrabajadorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
