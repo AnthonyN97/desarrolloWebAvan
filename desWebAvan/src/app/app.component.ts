de9666d3a8b6468537f9baea9a0cebd5740be289
Anthony Nova <anthony.nova@tecsup.edu.pe>
2022-11-06 20:09:56 -0500

    labo11 actualizado

--- desWebAvan/src/app/app.component.ts
+++ desWebAvan/src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, ViewChild} from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
 import { SelectornumericoComponent } from './selectornumerico/selectornumerico.component';
 
 @Component({
@@ -7,11 +6,46 @@ import { SelectornumericoComponent } from './selectornumerico/selectornumerico.c
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  @ViewChild('selector1') selector1!: SelectornumericoComponent;
-  @ViewChild('selector2') selector2!: SelectornumericoComponent;  
 
-  fijarSelector1(valor:number) {
+export class AppComponent {
+
+  //Dado
+  valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado: string = "";
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1 == this.valor2 && this.valor1 == this.valor3)
+      this.resultado = 'Ganó';
+    else
+      this.resultado = 'Perdió';
+  }
+
+  //Cronometro
+  mensaje = '';
+
+  actualizar(t: number) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }
+
+  //Selector Numerico
+  @ViewChild('selector1') selector1!: SelectornumericoComponent;
+  @ViewChild('selector2') selector2!: SelectornumericoComponent;
+
+  fijarSelector1(valor: number) {
     this.selector1.fijar(valor);
   }
 }
